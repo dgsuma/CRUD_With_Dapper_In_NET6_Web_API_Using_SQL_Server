4a157e72cb32e6307babe44e3c933be3b24af01b
Duminda <duminda@example.com>
2022-12-09 06:18:24 +0530

    chore: created 'SuperHeroController.cs' controller & its stub

---
+++ DapperCrud/Controllers/SuperHeroController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public SuperHeroController(IConfiguration config)
+        {
+            _config = config;
+        }
+    }
+}
--- DapperCrud/SuperHero.cs
+++ DapperCrud/SuperHero.cs
@@ -2,5 +2,10 @@
 {
     public class SuperHero
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
     }
 }
